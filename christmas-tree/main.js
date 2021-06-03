4e1e2268466123eda98c9688848f9d90626f2210
Miljana3010 <75883984+Miljana3010@users.noreply.github.com>
2021-06-03 17:50:01 +0200

    Add files via upload

---
+++ christmas-tree/main.js
@@ -0,0 +1,10 @@
+var x = 4;
+for (let i = 0; i < x; i++) {
+  for (let j = x - 1; j > i; j--) {
+    document.write("&nbsp&nbsp");
+  }
+  for (let k = 0; k <= (i * 2); k++) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
